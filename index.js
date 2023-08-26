de47b72b1900d352637642fa4360f9f95aa883a7
Guterresthomas <guterresmikael@gmail.com>
2023-08-26 17:00:43 -0300

    as

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const path = require('path');
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const router = express.Router();
+const cors = require('cors'); 
+
+
 const http = require('http');
 
-const { Client } = require('pg');
+
+
+//const authRoutes = require('./routes/auth');
+//app.use('/auth', authRoutes);
 
 
 const server = http.createServer((req, res) => {
@@ -11,12 +22,11 @@ const server = http.createServer((req, res) => {
 // Configurações de conexão com o banco de dados
 const client = new Client({
   user: 'postgres',
-  host: 'localhost',
-  database: 'loginadmin',
+  host: '127.0.0.1',
+  database: 'postgres',
   password: '1234',
   port: 5432, // Porta padrão do PostgreSQL
 });
-
 // Conecta ao banco de dados
 client.connect();
 
@@ -30,10 +40,87 @@ client.query('SELECT * FROM usuarios', (err, res) => {
   client.end(); // Fecha a conexão
 });
 
+app.use(cors({
+  origin: 'http://localhost:3002' // Substitua pelo seu domínio React
+}));
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  const { username, senha } = req.body;
+
+  // Consulta para verificar o login
+  client.query(
+    'SELECT * FROM usuarios WHERE username = $admin AND senha = $admin1234',
+    [username, senha],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erro de servidor');
+      } else {
+        if (result.rows.length === 1) {
+          res.status(200).json({ message: 'Login bem-sucedido' });
+        } else {
+          res.status(401).json({ message: 'Credenciais inválidas' });
+        }
+      }
+    }
+  );
+});
+
+const client2 = new Client({
+    user: 'usuario_verde',
+    host: '172.27.32.199',
+    database: 'VERDE', // Nome do segundo banco de dados
+    password: 'fou3%sdf',
+    port: 5432, // Porta padrão do PostgreSQL
+  });
+  client2.connect();
+  
+  client2.query('SELECT * FROM information_schema.tables', (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res.rows);
+  }
+   // Fecha a conexão
+});
+
+  app.use(express.urlencoded({extended: false}))
+  app.use(express.json());
+
+app.get('/databases', (req, res) => {
+  
+  // Conecta ao segundo banco de dados
+  client2.query(
+    "SELECT * FROM information_schema.tables WHERE table_schema = 'public'",
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        // Trate o erro e envie uma resposta de erro se necessário
+        res.status(500).json({ message: 'Erro de servidor' });
+        return;     
+      } else {
+        if (result.rows.length > 0) {
+          // Aqui, você pode processar os resultados da consulta
+          console.log(result.rows);
+          res.status(200).json(result.rows);
+        } else {
+          res.status(404).json({ message: 'Nenhuma tabela encontrada' });
+        }
+      }
+
+      client.end();
+    }
+  );
+  
+});
+
+
 
 
 
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
